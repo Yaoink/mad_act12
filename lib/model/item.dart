015623565a3e2a43973c3688bffb5aa4f514c76e
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2026-04-06 16:11:41 -0400

    create item model

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -1,5 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Item {
-  String id;
+  String? id;
   String name;
   int quantity;
+  double price;
+
+  Item({
+    this.id,
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+
+  //dart object to Firestore map
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'price': price,
+    };
+  }
+
+  //Firestore document to dart object
+  factory Item.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Item(
+      id: doc.id,
+      name: data['name'],
+      quantity: data['quantity'],
+      price: data['price'],
+    );
+  }
 }
